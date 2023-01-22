51d03c13871e4be85168036aec78d301261e4206
TanyaMaximova1 <tatamurom@gmail.com>
2023-01-22 23:27:56 +0300

    step

--- index.js
+++ index.js
@@ -17,81 +17,110 @@
 
 // const str2 = "abcdefghij";
 // console.log("(2,3): " + str2.substr(2, 3));
-let result;
-function replaceName() {
-    let fio = document.getElementById('fio').value; //строка на входе
-    let mass_fio = fio.split(' '); //разделение по пробелам
-    console.log(mass_fio); //вывод массива
 
-    let firstWord = mass_fio[0]; //переменная в которую записывается первое слово
-    let secondWord = mass_fio[1]; //переменная в которую записывается второе слово
-    let thirdWord = mass_fio[2]; //переменная в которую записывается третье слово
+//задание 1
+// let result;
+// function replaceName() {
+//     let fio = document.getElementById('fio').value; //строка на входе
+//     let mass_fio = fio.split(' '); //разделение по пробелам
+//     console.log(mass_fio); //вывод массива
 
-    firstWord = firstWord.toLowerCase();
-    secondWord = secondWord.toLowerCase();
-    thirdWord = thirdWord.toLowerCase();
+//     let firstWord = mass_fio[0]; //переменная в которую записывается первое слово
+//     let secondWord = mass_fio[1]; //переменная в которую записывается второе слово
+//     let thirdWord = mass_fio[2]; //переменная в которую записывается третье слово
 
-    // firstWord = firstWord.trim(firstWord);
-    // secondWord = secondWord.trim(secondWord);
-    // thirdWord = thirdWord.trim(thirdWord);
+//     firstWord = firstWord.toLowerCase();
+//     secondWord = secondWord.toLowerCase();
+//     thirdWord = thirdWord.toLowerCase();
 
-    let firstWordfirstLetter = firstWord[0].toUpperCase();
-    firstWord = firstWord.replace(firstWord[0], firstWordfirstLetter);
-    console.log(firstWord);
+//     // firstWord = firstWord.trim(firstWord);
+//     // secondWord = secondWord.trim(secondWord);
+//     // thirdWord = thirdWord.trim(thirdWord);
 
-    let secondWordfirstLetter = secondWord[0].toUpperCase();
-    secondWord = secondWord.replace(secondWord[0], secondWordfirstLetter);
-    console.log(secondWord);
+//     let firstWordfirstLetter = firstWord[0].toUpperCase();
+//     firstWord = firstWord.replace(firstWord[0], firstWordfirstLetter);
+//     console.log(firstWord);
 
-    let thirdWordfirstLetter = thirdWord[0].toUpperCase();
-    thirdWord = thirdWord.replace(thirdWord[0], thirdWordfirstLetter);
-    console.log(thirdWord);
+//     let secondWordfirstLetter = secondWord[0].toUpperCase();
+//     secondWord = secondWord.replace(secondWord[0], secondWordfirstLetter);
+//     console.log(secondWord);
 
-    result = `${firstWord} ${secondWord} ${thirdWord}`;
-    document.getElementById('fio').value = result;
+//     let thirdWordfirstLetter = thirdWord[0].toUpperCase();
+//     thirdWord = thirdWord.replace(thirdWord[0], thirdWordfirstLetter);
+//     console.log(thirdWord);
 
-    document.getElementById('yourName').value = result;
-    document.getElementById('yourName').readOnly = true;
+//     result = `${firstWord} ${secondWord} ${thirdWord}`;
+//     document.getElementById('fio').value = result;
 
-    function image() {
-        let img_link = document.getElementById('img_link').value; //ссылка на изображение
-        document.getElementById('img').src = img_link;
+//     document.getElementById('yourName').value = result;
+//     document.getElementById('yourName').readOnly = true;
 
-        document.getElementById('img_link').value;
-    }
-    image();
+//     function image() {
+//         let img_link = document.getElementById('img_link').value; //ссылка на изображение
+//         document.getElementById('img').src = img_link;
 
-    function checkSpam() {
-        let comment = document.getElementById('comment').value; //комментарий пользователя
-        let cleanComm;
-        // let cleanComm = comment.replace("ViAgRA", "***");
-        // cleanComm = comment.replace("xxx", "***");
+//         document.getElementById('img_link').value;
+//     }
+//     image();
 
-        // let spam1 = "viagra";
-        // let spam2 = "xxx";
+//     function checkSpam() {
+//         let comment = document.getElementById('comment').value; //комментарий пользователя
+//         let cleanComm;
+//         // let cleanComm = comment.replace("ViAgRA", "***");
+//         // cleanComm = comment.replace("xxx", "***");
 
-        // if (result.toLowerCase() == spam1 || result.toLowerCase() == spam2) {
-        //     console.log(true);
-        // }
+//         // let spam1 = "viagra";
+//         // let spam2 = "xxx";
 
-        comment = comment.toLowerCase();
+//         // if (result.toLowerCase() == spam1 || result.toLowerCase() == spam2) {
+//         //     console.log(true);
+//         // }
+
+//         comment = comment.toLowerCase();
 
 
-        if (comment.includes('viagra')) {
-            cleanComm = comment.replace("viagra", "***");
-            document.getElementById('cleanCcomment').value = cleanComm;
-        }
-        else if (comment.includes('xxx')) {
-            cleanComm = comment.replace("xxx", "***");
-            document.getElementById('cleanCcomment').value = cleanComm;
-        }
-        else {
-            document.getElementById('cleanCcomment').value = comment;
-        }
-        // document.getElementById('cleanCcomment');
-        // console.log(cleanComm);
-    }
-    checkSpam();
+//         if (comment.includes('viagra')) {
+//             cleanComm = comment.replace("viagra", "***");
+//             document.getElementById('cleanCcomment').value = cleanComm;
+//         }
+//         else if (comment.includes('xxx')) {
+//             cleanComm = comment.replace("xxx", "***");
+//             document.getElementById('cleanCcomment').value = cleanComm;
+//         }
+//         else {
+//             document.getElementById('cleanCcomment').value = comment;
+//         }
+//         // document.getElementById('cleanCcomment');
+//         // console.log(cleanComm);
+//     }
+//     checkSpam();
+// }
+
+// replaceName();
+
+
+
+//задание под ***
+function deleteTags() {
+    let str31 = "<br>Добрый<col><embed>день,<hr>уважаемые<img>гости!";
+    // let strrr = "Добрый дань!";
+    // let cleanStr;
+    // if (str31.include('<br>') || str31.include('<col>') || str31.include('<embed>') || str31.include('<hr>') || str31.include('<img>')) {
+    //     cleanStr = str31.replace("<br>", " ");
+    // }
+    mas = ["<br>", "<col>", "<embed>", "<hr>", "<img>"];
+
+    // for (var i = 0; i < length; i++) {
+    //     element = arr[i];
+    //     str31 = str31.replace(element, ' ');
+
+    //     // Do something with element
+    // }
+    // strrr = strrr.replace('дань', 'день');
+    str31 = str31.replace("<br>", ' ');
+
+    console.log(str31);
 }
 
-replaceName();
+deleteTags();
+// console.log(deleteTags(str));

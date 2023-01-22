e7a82c68e62ddc99ffd44be3fdc8468183c868cb
TanyaMaximova1 <tatamurom@gmail.com>
2023-01-22 23:28:37 +0300

    step1

--- index.js
+++ index.js
@@ -1,102 +1,82 @@
-// let str = 'таня';
-// let firstLetter = str[0].toUpperCase();
-// str = str.replace(str[0], firstLetter);
-// console.log(str);
-
-// console.log(Math.random() * 100);
-
-// const x = "Солнце";
-// console.log("The string length is " + x.length)
-
-// const upperText = "ВелоСипед";
-// document.write(upperText.toUpperCase())
-
-// let str1 = "розы такие розы";
-// str1 = str1.replace("розы", "красивые")
-// console.log(str1);
-
-// const str2 = "abcdefghij";
-// console.log("(2,3): " + str2.substr(2, 3));
-
 //задание 1
-// let result;
-// function replaceName() {
-//     let fio = document.getElementById('fio').value; //строка на входе
-//     let mass_fio = fio.split(' '); //разделение по пробелам
-//     console.log(mass_fio); //вывод массива
+let result;
+function replaceName() {
+    let fio = document.getElementById('fio').value; //строка на входе
+    let mass_fio = fio.split(' '); //разделение по пробелам
+    console.log(mass_fio); //вывод массива
 
-//     let firstWord = mass_fio[0]; //переменная в которую записывается первое слово
-//     let secondWord = mass_fio[1]; //переменная в которую записывается второе слово
-//     let thirdWord = mass_fio[2]; //переменная в которую записывается третье слово
+    let firstWord = mass_fio[0]; //переменная в которую записывается первое слово
+    let secondWord = mass_fio[1]; //переменная в которую записывается второе слово
+    let thirdWord = mass_fio[2]; //переменная в которую записывается третье слово
 
-//     firstWord = firstWord.toLowerCase();
-//     secondWord = secondWord.toLowerCase();
-//     thirdWord = thirdWord.toLowerCase();
+    firstWord = firstWord.toLowerCase();
+    secondWord = secondWord.toLowerCase();
+    thirdWord = thirdWord.toLowerCase();
 
-//     // firstWord = firstWord.trim(firstWord);
-//     // secondWord = secondWord.trim(secondWord);
-//     // thirdWord = thirdWord.trim(thirdWord);
+    // firstWord = firstWord.trim(firstWord);
+    // secondWord = secondWord.trim(secondWord);
+    // thirdWord = thirdWord.trim(thirdWord);
 
-//     let firstWordfirstLetter = firstWord[0].toUpperCase();
-//     firstWord = firstWord.replace(firstWord[0], firstWordfirstLetter);
-//     console.log(firstWord);
+    let firstWordfirstLetter = firstWord[0].toUpperCase();
+    firstWord = firstWord.replace(firstWord[0], firstWordfirstLetter);
+    console.log(firstWord);
 
-//     let secondWordfirstLetter = secondWord[0].toUpperCase();
-//     secondWord = secondWord.replace(secondWord[0], secondWordfirstLetter);
-//     console.log(secondWord);
+    let secondWordfirstLetter = secondWord[0].toUpperCase();
+    secondWord = secondWord.replace(secondWord[0], secondWordfirstLetter);
+    console.log(secondWord);
 
-//     let thirdWordfirstLetter = thirdWord[0].toUpperCase();
-//     thirdWord = thirdWord.replace(thirdWord[0], thirdWordfirstLetter);
-//     console.log(thirdWord);
+    let thirdWordfirstLetter = thirdWord[0].toUpperCase();
+    thirdWord = thirdWord.replace(thirdWord[0], thirdWordfirstLetter);
+    console.log(thirdWord);
 
-//     result = `${firstWord} ${secondWord} ${thirdWord}`;
-//     document.getElementById('fio').value = result;
+    result = `${firstWord} ${secondWord} ${thirdWord}`;
+    document.getElementById('fio').value = result;
 
-//     document.getElementById('yourName').value = result;
-//     document.getElementById('yourName').readOnly = true;
+    document.getElementById('yourName').value = result;
+    document.getElementById('yourName').readOnly = true;
 
-//     function image() {
-//         let img_link = document.getElementById('img_link').value; //ссылка на изображение
-//         document.getElementById('img').src = img_link;
+    function image() {
+        let img_link = document.getElementById('img_link').value; //ссылка на изображение
+        document.getElementById('img').src = img_link;
 
-//         document.getElementById('img_link').value;
-//     }
-//     image();
+        document.getElementById('img_link').value;
+    }
+    image();
 
-//     function checkSpam() {
-//         let comment = document.getElementById('comment').value; //комментарий пользователя
-//         let cleanComm;
-//         // let cleanComm = comment.replace("ViAgRA", "***");
-//         // cleanComm = comment.replace("xxx", "***");
+    function checkSpam() {
+        let comment = document.getElementById('comment').value; //комментарий пользователя
+        let cleanComm;
+        // let cleanComm = comment.replace("ViAgRA", "***");
+        // cleanComm = comment.replace("xxx", "***");
 
-//         // let spam1 = "viagra";
-//         // let spam2 = "xxx";
+        // let spam1 = "viagra";
+        // let spam2 = "xxx";
 
-//         // if (result.toLowerCase() == spam1 || result.toLowerCase() == spam2) {
-//         //     console.log(true);
-//         // }
+        // if (result.toLowerCase() == spam1 || result.toLowerCase() == spam2) {
+        //     console.log(true);
+        // }
 
-//         comment = comment.toLowerCase();
+        comment = comment.toLowerCase();
 
 
-//         if (comment.includes('viagra')) {
-//             cleanComm = comment.replace("viagra", "***");
-//             document.getElementById('cleanCcomment').value = cleanComm;
-//         }
-//         else if (comment.includes('xxx')) {
-//             cleanComm = comment.replace("xxx", "***");
-//             document.getElementById('cleanCcomment').value = cleanComm;
-//         }
-//         else {
-//             document.getElementById('cleanCcomment').value = comment;
-//         }
-//         // document.getElementById('cleanCcomment');
-//         // console.log(cleanComm);
-//     }
-//     checkSpam();
-// }
+        if (comment.includes('viagra')) {
+            cleanComm = comment.replace("viagra", "***");
+            document.getElementById('cleanCcomment').value = cleanComm;
+        }
+        else if (comment.includes('xxx')) {
+            cleanComm = comment.replace("xxx", "***");
+            document.getElementById('cleanCcomment').value = cleanComm;
+        }
+        else {
+            document.getElementById('cleanCcomment').value = comment;
+        }
+        // document.getElementById('cleanCcomment');
+        // console.log(cleanComm);
+    }
+    checkSpam();
+}
 
-// replaceName();
+replaceName();
 
 
 
